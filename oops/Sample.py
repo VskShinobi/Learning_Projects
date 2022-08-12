5270da3534083068594478203bce4e6e3f34829f
vsk <drop2vsureshkumar@gmail.com>
2022-08-12 12:02:31 +0530

    Sample work

--- oops/Sample.py
+++ oops/Sample.py
@@ -1,10 +1,19 @@
 #  Here i used the file for the sample project
 
-class Mobile():
-    pass
+class Mobile:
+    def __init__(self, brand, price):
+        print("Inside constructor")
+        self.brand = brand
+        self.price = price
+
+    def purchase(self):
+        print("Purchasing a mobile")
+        print("This mobile has brand", self.brand, "and price", self.price)
 
 
-modile1 = Mobile()
-modile1.price = 10000
-
-print(modile1.price)
+print("Mobile-1")
+mob1 = Mobile("Apple", 20000)
+mob1.purchase()
+print("Mobile-2")
+mob2 = Mobile("Samsung", 3000)
+mob2.purchase()
